9d6d03a01ec952242d9e6a2e58626a6038570bad
sagr24 <sagrawal26@sapient.com>
2017-05-11 18:35:23 +0530

    Shikha created project.

---
+++ sadpExercise/src/main/java/com/sadp/SadpExerciseApplication.java
@@ -0,0 +1,12 @@
+package com.sadp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadpExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SadpExerciseApplication.class, args);
+	}
+}
